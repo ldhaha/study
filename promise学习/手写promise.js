b541d5469bcc34a6bcc9564f275e4795456afec1
ldhaha <1025414131@qq.com>
2024-08-23 21:20:57 +0800

    异步then返回promise

--- promise学习/手写promise.js
+++ promise学习/手写promise.js
@@ -1,63 +1,64 @@
 function myPromise(executor) {
-  this.PromiseState = 'pending';
-  this.PromiseResult = undefined;
+  this.PromiseState = 'pending'
+  this.PromiseResult = undefined
   // this.callback = {}
-  this.callbacks = [];
-  const self = this;
+  this.callbacks = []
+  const self = this
   // resolve函数
   function resolve(value) {
-    if (self.PromiseState !== 'pending') return;
-    self.PromiseState = 'fullfilled';
-    self.PromiseResult = value;
+    if (self.PromiseState !== 'pending') return
+    self.PromiseState = 'fullfilled'
+    self.PromiseResult = value
     //1修改状态 promiseState
     //2修改结果值 primiseResult
     // self.callback.onResolved && self.callback.onResolved(value)
     self.callbacks.forEach((item) => {
-      item.onResolved(value);
-    });
+      item.onResolved(value)
+    })
   }
   // reject函数
   function reject(reason) {
-    if (self.PromiseState !== 'pending') return;
-    self.PromiseResult = reason;
-    self.PromiseState = 'rejected';
+    if (self.PromiseState !== 'pending') return
+    self.PromiseResult = reason
+    self.PromiseState = 'rejected'
     // self.callback.onRejected && self.callback.onRejected(value)
     self.callbacks.forEach((item) => {
-      item.onRejected(value);
-    });
+      item.onRejected(reason)
+    })
   }
   try {
-    executor(resolve, reject);
+    executor(resolve, reject)
   } catch (e) {
-    reject(e);
+    reject(e)
   }
 }
 
 myPromise.prototype.then = function (onResolved, onRejected) {
+  const self = this
   return new myPromise((resolve, reject) => {
     if (this.PromiseState === 'fullfilled') {
       //获取执行结果
       try {
-        let result = onResolved(this.PromiseResult);
+        let result = onResolved(this.PromiseResult)
         if (result instanceof myPromise) {
           result.then(
             (v) => {
-              resolve(v);
+              resolve(v)
             },
             (r) => {
-              reject(r);
+              reject(r)
             }
-          );
+          )
         } else {
           // 非promsie都是成功
-          resolve(result);
+          resolve(result)
         }
       } catch (e) {
-        reject(e);
+        reject(e)
       }
     }
     if (this.PromiseState === 'rejected') {
-      onRejected(this.PromiseResult);
+      onRejected(this.PromiseResult)
     }
     if (this.PromiseState === 'pending') {
       // 异步的时候，需要保存回调函数
@@ -66,19 +67,58 @@ myPromise.prototype.then = function (onResolved, onRejected) {
       //   onRejected
       // }
       this.callbacks.push({
-        onResolved,
-        onRejected
-      });
+        onResolved: function (value) {
+          try {
+            const res = onResolved(value)
+            console.log(res)
+            console.log('_____')
+            if (res instanceof myPromise) {
+              res.then(
+                (v) => {
+                  console.log('vvv')
+                  console.log(v)
+                  resolve(v)
+                },
+                (r) => reject(r)
+              )
+            } else {
+              resolve(res)
+            }
+          } catch (e) {
+            reject(e)
+          }
+        },
+        onRejected: function (reason) {
+          try {
+            const res = onRejected(reason)
+            if (res instanceof myPromise) {
+              res.then(
+                (v) => {
+                  resolve(v)
+                },
+                (r) => reject(r)
+              )
+            } else {
+              resolve(res)
+            }
+          } catch (e) {
+            reject(e)
+          }
+        }
+      })
     }
-  });
-};
+  })
+}
 
 const p = new myPromise((resolve, reject) => {
-  resolve('a');
-});
+  setTimeout(() => {
+    resolve('a')
+  }, 2000)
+})
 
 p.then((res) => {
-  console.log(res);
-}).then((a) => {
-  console.log(a);
-});
+  console.log('第一个then' + res)
+  return new myPromise((resolve, reject) => {
+    resolve('cccc')
+  })
+}).then((val) => console.log('第二个then' + val))
