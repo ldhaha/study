b6e28fa32ab7fde456ee98febcf63aaf4cd4a573
ldhaha <1025414131@qq.com>
2024-08-23 09:48:52 +0800

    promise手写复习

---
+++ promise学习/review.js
@@ -0,0 +1,87 @@
+function myPromise(executor) {
+    this.PromiseResult = undefined;
+    this.PromiseStatus = 'pending';
+    const self = this;
+    /**
+     * 解决异步resolve时，此时状态依然是pending,所以then不会执行，
+     * 这时候需要收集起来，执行resolve的时候再执行
+     * 定义为数组的原因是为了多次then
+     */
+    this.callbacks = [];
+
+    function resolve(value) {
+        if (self.PromiseStatus === 'pending') {
+            self.PromiseResult = value;
+            self.PromiseStatus = 'fullfilled';
+            self.callbacks.forEach((_) => _.onResolved(value));
+        }
+    }
+
+    function reject(reason) {
+        if (self.PromiseStatus === 'pending') {
+            self.PromiseResult = reason;
+            self.PromiseStatus = 'rejected';
+            self.callbacks.forEach((_) => _.onRejected(value));
+        }
+    }
+
+    /**
+     * new Promise里报错也会变成reject
+     */
+    try {
+        executor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+myPromise.prototype.then = function (onResolved, onRejected) {
+    // then会返回一个promise
+    return new Promise((resolve, reject) => {
+        if (this.PromiseStatus === 'fullfilled') {
+            try {
+                const res = onResolved(this.PromiseResult);
+                if (res instanceof myPromise) {
+                    res.then(
+                        (v) => resolve(v),
+                        (r) => reject(r)
+                    );
+                } else {
+                    resolve(res);
+                }
+            } catch (e) {
+                reject(e);
+            }
+        }
+        if (this.PromiseStatus === 'rejected') {
+            onRejected(this.PromiseResult);
+        }
+        if (this.PromiseStatus === 'pending') {
+            this.callbacks.push({
+                onResolved,
+                onRejected
+            });
+        }
+    });
+};
+
+const p = new myPromise((resolve, reject) => {
+    resolve(2);
+});
+p.then((value) => {
+    console.log(value);
+    return 'asdasg';
+}).then((res) => {
+    console.log(res);
+});
+
+const tp = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('2');
+    }, 2000);
+});
+tp.then((res) => {
+    console.log(res);
+}).then((value) => {
+    console.log(value);
+});
--- promise学习/手写promise.js
+++ promise学习/手写promise.js
@@ -1,35 +1,35 @@
 function myPromise(executor) {
-  this.PromiseState = 'pending'
-  this.PromiseResult = undefined
+  this.PromiseState = 'pending';
+  this.PromiseResult = undefined;
   // this.callback = {}
-  this.callbacks = []
-  const self = this
+  this.callbacks = [];
+  const self = this;
   // resolve函数
   function resolve(value) {
-    if (self.PromiseState !== 'pending') return
-    self.PromiseState = 'fullfilled'
-    self.PromiseResult = value
+    if (self.PromiseState !== 'pending') return;
+    self.PromiseState = 'fullfilled';
+    self.PromiseResult = value;
     //1修改状态 promiseState
     //2修改结果值 primiseResult
     // self.callback.onResolved && self.callback.onResolved(value)
     self.callbacks.forEach((item) => {
-      item.onResolved(value)
-    })
+      item.onResolved(value);
+    });
   }
   // reject函数
   function reject(reason) {
-    if (self.PromiseState !== 'pending') return
-    self.PromiseResult = reason
-    self.PromiseState = 'rejected'
+    if (self.PromiseState !== 'pending') return;
+    self.PromiseResult = reason;
+    self.PromiseState = 'rejected';
     // self.callback.onRejected && self.callback.onRejected(value)
     self.callbacks.forEach((item) => {
-      item.onRejected(value)
-    })
+      item.onRejected(value);
+    });
   }
   try {
-    executor(resolve, reject)
+    executor(resolve, reject);
   } catch (e) {
-    reject(e)
+    reject(e);
   }
 }
 
@@ -38,26 +38,26 @@ myPromise.prototype.then = function (onResolved, onRejected) {
     if (this.PromiseState === 'fullfilled') {
       //获取执行结果
       try {
-        let result = onResolved(this.PromiseResult)
+        let result = onResolved(this.PromiseResult);
         if (result instanceof myPromise) {
           result.then(
             (v) => {
-              resolve(v)
+              resolve(v);
             },
             (r) => {
-              reject(r)
+              reject(r);
             }
-          )
+          );
         } else {
           // 非promsie都是成功
-          resolve(result)
+          resolve(result);
         }
       } catch (e) {
-        reject(e)
+        reject(e);
       }
     }
     if (this.PromiseState === 'rejected') {
-      onRejected(this.PromiseResult)
+      onRejected(this.PromiseResult);
     }
     if (this.PromiseState === 'pending') {
       // 异步的时候，需要保存回调函数
@@ -68,22 +68,17 @@ myPromise.prototype.then = function (onResolved, onRejected) {
       this.callbacks.push({
         onResolved,
         onRejected
-      })
+      });
     }
-  })
-}
+  });
+};
 
 const p = new myPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('a')
-  }, 1000)
-})
+  resolve('a');
+});
 
-p.then(
-  (value) => {
-    console.log(value)
-  },
-  (reason) => {
-    console.warn(reason)
-  }
-)
+p.then((res) => {
+  console.log(res);
+}).then((a) => {
+  console.log(a);
+});
