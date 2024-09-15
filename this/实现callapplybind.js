565afd7de452cdb8d7bb91300f1721d511c400b0
ldhaha <1025414131@qq.com>
2024-09-15 16:04:24 +0800

    bind

--- this/实现callapplybind.js
+++ this/实现callapplybind.js
@@ -2,12 +2,37 @@
 Function.prototype.myCall = function (thisArg, ...args) {
   const fn = this
   // 防止传入基本类型 数字/字符串/布尔,undefined,null永远是window
-  thisArg = thisArg ? Object(thisArg) : window
+  thisArg = thisArg !== null && thisArg !== undefined ? Object(thisArg) : window
   thisArg.fn = fn
   return thisArg.fn(...args) // 展开
   delete thisArg.fn
 }
 
+// apply
+Function.prototype.myApply = function (thisArg, argArray) {
+  const fn = this
+  // 防止传入基本类型 数字/字符串/布尔,undefined,null永远是window
+  thisArg = thisArg !== null && thisArg !== undefined ? Object(thisArg) : window
+
+  thisArg.fn = fn
+  argArray = argArray || []
+  return thisArg.fn(...argArray) // 展开
+  delete thisArg.fn
+}
+
+// bind
+Function.prototype.myBind = function (thisArg, ...args) {
+  const fn = this
+  // 防止传入基本类型 数字/字符串/布尔,undefined,null永远是window
+  thisArg = thisArg !== null && thisArg !== undefined ? Object(thisArg) : window
+
+  return function (...e) {
+    thisArg.fn = fn
+    let result = thisArg.fn(...[...args, ...e])
+    delete thisArg.fn
+    return result
+  } // 展开
+}
 function foo() {
   console.log('haha', this)
 }
