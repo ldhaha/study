83a94b9505dcb240c3dad3e92e1a3030d5cb644d
ldhaha <1025414131@qq.com>
2024-09-16 15:22:37 +0800

    组合函数

---
+++ js函数式编程/组合函数.js
@@ -0,0 +1,18 @@
+function Compose(...fns) {
+  const length = fns.length
+  for (let i = 0; i < length; i++) {
+    if (typeof fns[i] !== 'function') {
+      throw new TypeError()
+    }
+  }
+
+  function compose(...args) {
+    let index = 0
+    let result = length ? fns[index].apply(this, args) : args
+    while (++index < length) {
+      result = fns[index].call(this, result)
+    }
+    return result
+  }
+  return compose
+}
