5310756538e432152fc338f044abfd860e721b63
ldhaha <1025414131@qq.com>
2024-10-20 15:21:36 +0800

    optimize

--- 工程化/webpack-optimize/src/index.js
+++ 工程化/webpack-optimize/src/index.js
@@ -1,1 +1,6 @@
+import axios from 'axios'
+import _ from 'lodash'
+
+axios.get('http://www.baidu.com')
 console.log('webpack optimize')
+console.log(_.cloneDeep)
--- 工程化/webpack-optimize/webpack.config.js
+++ 工程化/webpack-optimize/webpack.config.js
@@ -1,20 +1,63 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { output, plugins, devServer } = require('../babel/webpack.config')
+const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  // 多入口打包
+  entry: {
+    index: {
+      import: './src/index.js',
+
+      // 用到共享的，实验了 只能写一个
+
+      dependOn: 'shared'
+    },
+    main: {
+      import: './src/main.js'
+    },
+
+    // 共享的第三方库
+    shared: ['axios']
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
+    /** 分包的命名（结合webpackChunkName） */
+    chunkFilename: '[name].chunk.js',
+    clean: true,
+
+    // index.html引入的打包后的文件地址会加前缀（例如cdn）
+    publicPath: './' // or  http://www.baidu.com(举例)
   },
   mode: 'development',
+  devServer: {
+    open: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
     })
   ],
-  devServer: {
-    open: true
+
+  // 生产环境分包（优化）
+  optimization: {
+    splitChunks: {
+      // development:named, production:deterministic
+      // chunkId: '',
+      // 默认async  只会将动态导入的分包
+      chunks: 'all',
+      // // 一个包大于这个数值时，会接着分，但是也不一定，主要有的功能很大，只能连起来写
+      // maxSize: 20000,
+
+      // // 每个包最小的大小
+      // minSize: 10000,
+
+      // 自定义哪些包需要拆出来
+      cacheGroups: {
+        axios: {
+          test: /[\\/]node_modules[\\/]/,
+          filename: 'abc_vendor.js'
+        }
+      }
+    }
   }
 }
