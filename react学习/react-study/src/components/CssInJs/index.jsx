99559f98d8045a8ae4b3c27e4293d10529d74d83
ldhaha <1025414131@qq.com>
2024-12-17 09:20:23 +0800

    classnames

--- react学习/react-study/src/components/CssInJs/index.jsx
+++ react学习/react-study/src/components/CssInJs/index.jsx
@@ -1,9 +1,11 @@
 import { AppWrapper } from "./style";
+import classnames from "classnames";
 export function CssInJs() {
   return (
     <>
-      <AppWrapper color="purple">
+      <AppWrapper color="purple" fontSize="20px">
         <div className="title">css in js</div>
+        <div className={classnames("ld", { cl: false })}>classnames</div>
       </AppWrapper>
     </>
   );
--- react学习/react-study/src/components/CssInJs/style.js
+++ react学习/react-study/src/components/CssInJs/style.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
-
-export const AppWrapper = styled.div`
+const outVariable = "bold";
+// attrs 设置默认值
+export const AppWrapper = styled.div.attrs((props) => {
+  return {
+    tSize: props.fontSize || "36px",
+  };
+})`
   .title {
     color: ${(props) => props.color};
+    font-size: ${(props) => props.tSize};
+    font-weight: ${outVariable};
   }
 `;
