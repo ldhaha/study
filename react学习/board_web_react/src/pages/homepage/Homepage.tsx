ad2595d5898e161fd999449b3fd31cfad19b7024
ldhaha <1025414131@qq.com>
2024-12-24 09:23:56 +0800

    数据模拟

--- react学习/board_web_react/src/pages/homepage/Homepage.tsx
+++ react学习/board_web_react/src/pages/homepage/Homepage.tsx
@@ -1,28 +1,50 @@
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import sytleModule from './style.module.css';
 import CardCom from './components/cardCom/CardCom';
+import { leftCardData } from './mockData';
+type CardDataType = {
+  name: string;
+  date: string;
+};
 const Homepage = memo(() => {
   const [ltIndex, setLtIndex] = useState(0);
   const [rtIndex, setRtIndex] = useState(0);
+  const [ltLoading, setLtLoading] = useState(false);
+  const [rtLoading] = useState(false);
+  const [leftData, setLeftData] = useState<CardDataType[]>([]);
   const leftTopTabList = [
     {
-      name: '公司公告'
+      name: '火影忍者'
     },
     {
-      name: '政策法规'
+      name: '七龙珠'
     }
   ];
   const rightTopTabList = [
     {
-      name: '风险监控'
+      name: '铠甲勇士'
     },
     {
-      name: '研究报告'
+      name: '镇魂街'
     },
     {
-      name: '违规案例'
+      name: '请回答1998'
     }
   ];
+
+  const generateLeftData = (index: number) => {
+    setLtLoading(true);
+    setLtIndex(index);
+    const data = leftCardData[index];
+    setTimeout(() => {
+      setLeftData(data);
+      setLtLoading(false);
+    }, 500);
+  };
+
+  useEffect(() => {
+    generateLeftData(0);
+  }, []);
   return (
     <div className='w-[1400px] mx-auto pt-[11px]'>
       <div className={`h-[194px] ${sytleModule.banner} mb-[7px]`}></div>
@@ -30,15 +52,16 @@ const Homepage = memo(() => {
         <CardCom
           tabList={leftTopTabList}
           currentIndex={ltIndex}
-          setCurrent={setLtIndex}
-        >
-          <div>1</div>
-          <div>2</div>
-        </CardCom>
+          setCurrent={generateLeftData}
+          loading={ltLoading}
+          data={leftData}
+        ></CardCom>
         <CardCom
           tabList={rightTopTabList}
           currentIndex={rtIndex}
           setCurrent={setRtIndex}
+          loading={rtLoading}
+          data={[]}
         />
       </div>
     </div>
--- react学习/board_web_react/src/pages/homepage/components/cardCom/CardCom.tsx
+++ react学习/board_web_react/src/pages/homepage/components/cardCom/CardCom.tsx
@@ -1,33 +1,47 @@
-import { memo } from "react";
-import styleModule from "./style.module.css";
+import { memo } from 'react';
+import styleModule from './style.module.css';
+import { Spin } from '@arco-design/web-react';
 interface PropsType {
   tabList: { name: string }[];
   currentIndex: number;
   setCurrent: Function;
-  children?: JSX.Element[];
+  loading: boolean;
+  data: { name: string; date: string }[];
 }
 const CardCom = memo((props: PropsType) => {
-  const { tabList, currentIndex, setCurrent, children } = props;
+  const { tabList, currentIndex, setCurrent, loading, data } = props;
   return (
-    <div className={styleModule.cardWrapper}>
-      <div className="flex justify-between">
-        <div className="tab-list">
-          {tabList.map((item, index) => (
-            <span
-              className={`text-[#07172b] mr-[40px] cursor-pointer ${
-                currentIndex === index ? "!text-primary" : ""
-              } `}
+    <Spin loading={loading}>
+      <div className={styleModule.cardWrapper}>
+        <div className='flex justify-between mb-[20px]'>
+          <div className='tab-list'>
+            {tabList.map((item, index) => (
+              <span
+                className={`text-[#07172b] mr-[40px] cursor-pointer ${
+                  currentIndex === index ? '!text-primary' : ''
+                } `}
+                key={index}
+                onClick={() => setCurrent(index)}
+              >
+                {item.name}
+              </span>
+            ))}
+          </div>
+          <div className='text-[14px] text-primary'>查看更多</div>
+        </div>
+        <div>
+          {data.map((_, index) => (
+            <div
               key={index}
-              onClick={() => setCurrent(index)}
+              className='flex justify-between py-[12px] border-t-solid border-t-[#f5f6f7] border-t-[1px]'
             >
-              {item.name}
-            </span>
+              <span>{_.name}</span>
+              <span>{_.date}</span>
+            </div>
           ))}
         </div>
-        <div className="text-[14px] text-primary">查看更多</div>
       </div>
-      <div>{children && children.length && children[currentIndex]}</div>
-    </div>
+    </Spin>
   );
 });
 
